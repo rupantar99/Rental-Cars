4240fbbe954166c149a28a7a1002e0888072c02d
Rupantar Chowdhury <rupantarchowdhury99@gmail.com>
2023-09-19 03:47:33 +0530

    Authentication updated

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -17,6 +17,7 @@ function Hero() {
             <h2 className='text-[20px] text-gray-500 pr-20 mt-5'>Book the selected car effortlessly, Pay for driving only,
                 Book the Car Now
             </h2> */}
+
             <button className='p-2 mt-5 bg-blue-500 text-white
             px-4 rounded-full 
             hover:scale-105 transition-all'>Explore Cars</button>
